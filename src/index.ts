83a9df6adba3d6a7954936eed45463078aad9599
Fabian Klonsdorf <33894877+klonsian@users.noreply.github.com>
2021-07-21 22:06:41 +0200

    feat: header

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,34 @@ import CookieBanner from "./components/CookieBanner/CookieBanner";
 import Link from "./components/Link/Link";
 import LoginForm from "./components/LoginForm/LoginForm";
 import SnackBar from "./components/SnackBar/SnackBar";
+import AspectRatio from "./components/AspectRatio/AspectRatio";
+import CodeSnippet from "./components/CodeSnippet/CodeSnippet";
+import CopyButton from "./components/CopyButton/CopyButton";
+import { Grid, Column } from "./components/Grid/Grid";
+import Header from "./components/Header/Header";
+import LeadSpace from "./components/LeadSpace/LeadSpace";
+import Logo from "./components/Logo/Logo";
+import SideNav from "./components/SideNav/SideNav";
+import Tooltip from "./components/Tooltip/Tooltip";
+import Typography from "./components/Typography/Typography";
+import UserProfile from "./components/UserProfile/UserProfile";
 
-export { Button, Link, SnackBar, CookieBanner, LoginForm };
+export {
+  Button,
+  Link,
+  SnackBar,
+  CookieBanner,
+  LoginForm,
+  AspectRatio,
+  CodeSnippet,
+  CopyButton,
+  Grid,
+  Column,
+  Header,
+  LeadSpace,
+  Logo,
+  SideNav,
+  Tooltip,
+  Typography,
+  UserProfile
+};
