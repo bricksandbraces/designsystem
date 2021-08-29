84e13b1dbcfd83037a291f4c9368ad8faa360b23
Fabian Klonsdorf <33894877+klonsian@users.noreply.github.com>
2021-08-29 16:15:37 +0200

    fix: imports

--- src/components/Tabs/ResponsiveTabs.tsx
+++ src/components/Tabs/ResponsiveTabs.tsx
@@ -1,7 +1,4 @@
-import React, { useState, ReactNode, useRef, useEffect } from "react";
-import cx from "classnames";
-import Typography from "../Typography/Typography";
-import Tab, { TabProps } from "./Tab";
+import React, { ReactNode } from "react";
 import Tabs from "./Tabs";
 import Accordion from "../Accordion/Accordion";
 
