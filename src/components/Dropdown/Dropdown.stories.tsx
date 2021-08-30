01a0788880a3799ac79d0e6231e128f7c3ddc1e4
Fabian Klonsdorf <33894877+klonsian@users.noreply.github.com>
2021-08-30 21:48:18 +0200

    fix: dropdown

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -15,39 +15,45 @@ const defaultSize = "default";
 export const Default = () => {
   return (
     <div style={{ width: "100vw", height: "100vh", padding: "32px" }}>
-      <div style={{ width: "405px" }}>
-        <Dropdown
-          label={text("Label", "Checkbox label")}
-          size={select("Size", sizeOptions, defaultSize) as any}
-          items={object("Items", [
-            {
-              id: "option-0",
-              text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-              selected: true
-            },
-            {
-              id: "option-1",
-              text: "Option 1"
-            },
-            {
-              id: "option-2",
-              text: "Option 2"
-            },
-            {
-              id: "option-3",
-              text: "Option 3"
-            },
-            {
-              id: "option-4",
-              text: "Option 4"
-            },
-            {
-              id: "option-5",
-              text: "Option 5"
-            }
-          ])}
-        />
-      </div>
+      <Dropdown
+        title={text("Title", "Dropdown title")}
+        label={text("Label", "Dropdown label")}
+        size={select("Size", sizeOptions, defaultSize) as any}
+        warningText={text("warningText", "")}
+        errorText={text("errorText", "")}
+        items={object("Items", [
+          {
+            id: "option-0",
+            value: "option-0",
+            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          },
+          {
+            id: "option-1",
+            value: "option-1",
+            text: "Option 1"
+          },
+          {
+            id: "option-2",
+            value: "option-2",
+            text: "Option 2"
+          },
+          {
+            id: "option-3",
+            value: "option-3",
+            text: "Option 3"
+          },
+          {
+            id: "option-4",
+            value: "option-4",
+            text: "Option 4"
+          },
+          {
+            id: "option-5",
+            value: "option-5",
+            text: "Option 5"
+          }
+        ])}
+      />
     </div>
   );
 };
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import cx from "classnames";
-import { IconChevronDown } from "@tabler/icons";
+import {
+  IconAlertCircle,
+  IconAlertTriangle,
+  IconChevronDown
+} from "@tabler/icons";
+import Typography from "../Typography/Typography";
 
 type DropDownItem = {
   /**
    * Dropdown ID
    */
   id?: string;
+
+  /**
+   * Dropdown Value
+   */
+  value?: string;
 
   /**
    * Checkbox Label
@@ -36,22 +46,58 @@ type DropdownProps = {
   label?: string;
 
   /**
+   * Error
+   */
+  error?: boolean;
+  errorText?: string;
+
+  /**
+   * Warning
+   */
+  warning?: boolean;
+  warningText?: string;
+
+  /**
+   * Dropdown title
+   */
+  title: string;
+
+  /**
    * Dropdown items
    */
   items: DropDownItem[];
 };
 
-const Dropdown = ({ size, label, className, items }: DropdownProps) => {
+const Dropdown = ({
+  size,
+  label,
+  className,
+  title,
+  items,
+  error,
+  errorText,
+  warning,
+  warningText
+}: DropdownProps) => {
   const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
+  const [selectedText, setSelectedText] = useState(null);
   return (
     <>
+      <Typography type="span" token="body-small" className="dropdown--label">
+        {label}
+      </Typography>
       <button
+        aria-expanded={open}
         className={cx(
           "dropdown",
           {
             "dropdown--large": size === "large",
             "dropdown--small": size === "small",
-            "dropdown--default": size === "default"
+            "dropdown--default": size === "default",
+            "drodown--error": (error || errorText) && !(warning || warningText),
+            "dropdown--warning":
+              !(error || errorText) && (warning || warningText)
           },
           className
         )}
@@ -59,35 +105,65 @@ const Dropdown = ({ size, label, className, items }: DropdownProps) => {
           setOpen(!open);
         }}
       >
-        {label}
+        <Typography
+          type="span"
+          token="body-small"
+          className="dropdown--title"
+          title={selectedText}
+        >
+          {selectedValue === null ? title : selectedText}
+        </Typography>
         <IconChevronDown
           size={16}
           className={cx("dropdown--icon", { "dropdown--icon-open": open })}
         />
       </button>
-      <div className={cx("dropdown--menu", { "dropdown--menu-open": open })}>
+      {errorText && !warningText && (
+        <div className="dropdown--error-text">
+          <IconAlertCircle size={16} />
+
+          {errorText}
+        </div>
+      )}
+      {warningText && !errorText && (
+        <div className="dropdown--warning-text">
+          <IconAlertTriangle size={16} />
+
+          {warningText}
+        </div>
+      )}
+      <ul
+        role="menu"
+        className={cx("dropdown--menu", { "dropdown--menu-open": open })}
+        aria-hidden={open}
+      >
         {items.map((item, i) => {
           return (
-            <div
+            <li
               // eslint-disable-next-line react/no-array-index-key
               key={i}
               className={cx("dropdown--menu-item", {
                 "dropdown--large": size === "large",
                 "dropdown--small": size === "small",
                 "dropdown--default": size === "default",
-                "dropdown--menu-item__selected": item.selected
+                "dropdown--menu-item__selected": selectedValue === item.value
               })}
               id={item.id}
+              value={item.value}
+              title={item.text}
+              onClick={() => {
+                setSelectedValue(item.value),
+                  setOpen(false),
+                  setSelectedText(item.text);
+              }}
             >
               <div className="dropdown--menu-item__text">
-                <span title={item.text}>
-                  {item.text}
-                </span>
+                <span title={item.text}>{item.text}</span>
               </div>
-            </div>
+            </li>
           );
         })}
-      </div>
+      </ul>
     </>
   );
 };
