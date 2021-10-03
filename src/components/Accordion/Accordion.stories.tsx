f447ed65e2109c6380c0820831c6b25a4a4f1b77
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2021-10-03 12:49:39 +0200

    Bring back uncontrolled accordion story

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -6,6 +6,24 @@ import AccordionItem from "./AccordionItem";
 
 export default { title: "Components/Accordion", decorators: [withKnobs] };
 
+export const Uncontrolled = () => {
+  return (
+    <div style={{ marginTop: "16px" }}>
+      <Grid narrow>
+        <Column sm={4} md={8} lg={16} xlg={16}>
+          <Accordion>
+            <AccordionItem title="Title 1" disabled>
+              Content 1
+            </AccordionItem>
+            <AccordionItem title="Title 2">Content 2</AccordionItem>
+            <AccordionItem title="Title 3">Content 3</AccordionItem>
+          </Accordion>
+        </Column>
+      </Grid>
+    </div>
+  );
+};
+
 export const Controlled = () => {
   const [openIndices, setOpenIndices] = useState<number[]>([]);
   return (
