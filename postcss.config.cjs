7550cb634411854798496b222d9144f9307c5390
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2021-10-24 10:50:05 +0200

    PostCSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -5,19 +5,18 @@ module.exports = (ctx) => ({
     require("postcss-import")(require("postcss-normalize")().postcssImport()),
     require("postcss-preset-env"),
     require("postcss-flexbugs-fixes"),
-
     require("postcss-each")({
       plugins: {
         beforeEach: [
           require("postcss-mixins"),
           require("postcss-nested"),
           require("postcss-simple-vars"),
+          require("postcss-hexrgba"),
+          require("postcss-utilities"),
           require("postcss-calc")
         ]
       }
     }),
-    require("postcss-hexrgba"),
-    require("postcss-utilities"),
     require("postcss-viewport-height-correction"),
     require("postcss-size"),
     require("autoprefixer"),
