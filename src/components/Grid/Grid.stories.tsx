c20849b7681bbb422ea1e180bdb2c4cc6b18335a
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2021-10-26 19:19:32 +0200

    Rename story

--- src/components/Grid/Grid.stories.tsx
+++ src/components/Grid/Grid.stories.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { prefix } from "../../settings";
 import { Grid, Column } from "./Grid";
 
-export default { title: "Components/Grid", decorators: [withKnobs] };
+export default { title: "Components/A_REFA_Grid", decorators: [withKnobs] };
 
 export const Auto = () => {
   return (
