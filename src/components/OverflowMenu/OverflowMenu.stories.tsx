b887faae2c0e51ae257068108d0238cc0ea74569
Fabian Klonsdorf <33894877+klonsian@users.noreply.github.com>
2021-11-18 16:46:14 +0100

    fix: size options in storybook for om

--- src/components/OverflowMenu/OverflowMenu.stories.tsx
+++ src/components/OverflowMenu/OverflowMenu.stories.tsx
@@ -1,4 +1,4 @@
-import { withKnobs } from "@storybook/addon-knobs";
+import { select, withKnobs } from "@storybook/addon-knobs";
 import {
   Icon2fa,
   IconAccessible,
@@ -16,10 +16,18 @@ export default {
   decorators: [withKnobs]
 };
 
+const sizeOptions = {
+  Large: "large",
+  Small: "small",
+  Default: "default"
+};
+
+const defaultSize = "default";
+
 export const Default = () => {
   return (
     <div style={{ padding: "32px", display: "flex", gap: "16px" }}>
-      <OverflowMenu>
+      <OverflowMenu size={select("size", sizeOptions, defaultSize) as any}>
         <OverflowMenuItem icon={<Icon2fa />}>Enable 2FA</OverflowMenuItem>
         <OverflowMenuItem icon={<IconAccessible />}>
           Turn on accessibility
