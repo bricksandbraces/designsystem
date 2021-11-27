7f1bdcd9484fd990316ff800757612b0b5e1b8f1
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2021-11-27 15:42:56 +0100

    Documentation

--- src/helpers/mathUtilities.ts
+++ src/helpers/mathUtilities.ts
@@ -1,5 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 
+/**
+ * Math polyfill function to clamp a value between min and max.
+ *
+ * @param val The value to clamp
+ * @param min Lower border
+ * @param max Upper border
+ * @returns The clamped value
+ */
 export function clamp(val: number, min: number, max: number) {
   return Math.max(min, Math.min(val, max));
 }
--- src/helpers/numberUtilities.ts
+++ src/helpers/numberUtilities.ts
@@ -1,4 +1,12 @@
 /* eslint-disable import/prefer-default-export */
+
+/**
+ * Parse a number from a string.
+ *
+ * @param toParse the string to parse
+ * @param float if the string could be a float
+ * @returns the parsed number
+ */
 const parseToNumber = (toParse: string | undefined, float: boolean = false) => {
   if (toParse == null) return Number.NaN;
   const number = float ? Number.parseFloat(toParse) : Number.parseInt(toParse);
