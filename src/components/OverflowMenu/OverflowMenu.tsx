868ba50da214c3d03fb2c2ff22ffa3cdf09daf76
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2021-12-13 18:27:33 +0100

    Make tippies more typesafe

--- src/components/OverflowMenu/OverflowMenu.tsx
+++ src/components/OverflowMenu/OverflowMenu.tsx
@@ -1,65 +1,61 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import cx from "classnames";
 import { prefix } from "../../settings";
 import IconOnlyButton from "../Button/IconOnlyButton";
 import { IconDotsVertical } from "@tabler/icons";
+import type { TippyProps } from "@tippyjs/react";
 import Tippy from "@tippyjs/react";
 
-type OverflowMenuProps = {
+export type OverflowMenuProps = {
   /**
    * OverflowMenu Children
    */
-  children?: string | ReactNode;
-
-  /**
-   * OverflowMenu ClassName
-   */
-  className?: string;
+  children?: string | React.ReactNode;
 
   /**
    * OverflowMenu Icon
    */
-  icon?: ReactNode;
+  icon?: React.ReactNode;
 
   /**
    * OverflowMenu Size
    */
   size?: "large" | "default" | "small";
-};
+} & Omit<TippyProps, "content">;
 
-const OverflowMenu = ({
-  size = "default",
-  children,
-  icon = <IconDotsVertical />,
-  className,
-  ...props
-}: OverflowMenuProps) => {
+const OverflowMenu = (
+  {
+    size = "default",
+    children,
+    icon = <IconDotsVertical />,
+    className,
+    ...props
+  }: OverflowMenuProps,
+  ref: React.ForwardedRef<HTMLElement>
+) => {
   return (
-    <>
-      <Tippy
-        interactive
-        arrow={false}
-        className={cx(
-          `${prefix}--overflowmenu ${prefix}--overflowmenu-${size}`
-        )}
-        animation="bbds-animation"
-        trigger="click"
-        placement="bottom-start"
-        theme="dark"
-        {...props}
-        allowHTML
-        content={children}
-      >
-        <IconOnlyButton
-          icon={icon}
-          size={size}
-          kind="ghost"
-          hideTooltip
-          className={cx(`${prefix}--overflowmenu-trigger`)}
-        />
-      </Tippy>
-    </>
+    <Tippy
+      ref={ref}
+      interactive
+      arrow={false}
+      className={cx(`${prefix}--overflowmenu ${prefix}--overflowmenu-${size}`)}
+      animation="bbds-animation"
+      trigger="click"
+      placement="bottom-start"
+      theme="dark"
+      {...props}
+      allowHTML
+      content={children}
+    >
+      <IconOnlyButton
+        icon={icon}
+        size={size}
+        kind="ghost"
+        hideTooltip
+        className={cx(`${prefix}--overflowmenu-trigger`)}
+      />
+    </Tippy>
   );
 };
 
-export default OverflowMenu;
+export default React.forwardRef(OverflowMenu);
