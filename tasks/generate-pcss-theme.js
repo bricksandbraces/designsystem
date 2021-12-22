5fafceb9beae1a4be8d52b8bf768713f8d0c8156
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2021-12-22 09:18:06 +0100

    Automatic theming generation

--- tasks/generate-pcss-theme.js
+++ tasks/generate-pcss-theme.js
@@ -1,15 +1,22 @@
-"use strict";
-exports.__esModule = true;
-var fs_1 = require("fs");
-var theme_1 = require("../src/theme");
-var output = ":root {\n";
-Object.keys(theme_1["default"].light).forEach(function (key) {
-    output += "\t--" + key + ": " + theme_1["default"][key] + ";\n";
+const fs = require("fs");
+const importedTheme = require("../src/theme.js");
+
+let output =
+  "/** DO NOT MODIFY! This file has been auto-generated from task:generate-pcss-theme.\nFor changing, consolidate src/theme.js and rerun the task. */\n\n:root {\n";
+
+Object.keys(importedTheme.light).forEach((key) => {
+  output += "  --" + key + ": " + importedTheme.light[key] + ";\n";
 });
+
 output += "}\n\n";
-output += "@media (prefers-color-scheme: dark) {\n\t:root {\n\t\t\n";
-Object.keys(theme_1["default"].dark).forEach(function (key) {
-    output += "\t--" + key + ": " + theme_1["default"][key] + ";\n";
+output += "@media (prefers-color-scheme: dark) {\n  :root {\n";
+
+Object.keys(importedTheme.dark).forEach((key) => {
+  output += "    --" + key + ": " + importedTheme.dark[key] + ";\n";
 });
-output += "\n\t}\n}\n";
-(0, fs_1.writeFileSync)("../src/styles/themes.pcss", output);
+
+output += "\n  }\n}\n";
+
+console.log(output);
+
+fs.writeFileSync(__dirname + "/../src/styles/theme.pcss", output);
